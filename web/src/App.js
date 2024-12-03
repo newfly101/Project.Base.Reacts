71bb445381f48e4b331cf20fa1502b1b7841cae8
Jae-Hong-Kim <newfly101@naver.com>
2024-12-03 13:34:20 +0900

    [1] 할일 목록 삭제하기 (.filter() 사용)

--- web/src/App.js
+++ web/src/App.js
@@ -3,27 +3,54 @@ import './css/general.css';
 
 
 export default class App extends React.Component {
-    todoData = [
-        {
-            id: 1,
-            title: 'Todo 1',
-            completed: true,
-        },
-        {
-            id: 2,
-            title: 'Todo 2',
-            completed: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            todoData: [
+                { id: 1, title: 'Todo 1', completed: true },
+                { id: 2, title: 'Todo 2', completed: false }
+            ]
         }
-    ];
+        this.inputRefs = {};
+    }
     handleModify = (id) => {
-        console.log(id);
+        if (this.inputRefs[id]) {
+            this.inputRefs[id].focus();
+        }
     }
     handleDelete = (id) => {
-        let index = this.todoData.filter((data) => data.id !== id);
-        console.log(id);
+        let index = this.state.todoData.filter((data) => data.id !== id);
+        this.setState({todoData: index});
     }
-    handleOnChangeChecked = (e) => {
-        return e !== true;
+    handleOnChangeChecked = (e, id) => {
+        console.log(e.target.checked);
+        const newTodo = this.state.todoData.map((todo) => {
+            if (todo.id === id) {
+                return {...todo, completed: e.target.checked};
+            }
+            return todo;
+        });
+
+        this.setState({ todoData: newTodo });
+    }
+    handleOnChangeInput = (e, id) => {
+        console.log(e.target.value);
+        const newTodo = this.state.todoData.map((todo) => {
+            if (todo.id === id) {
+                return {...todo, title: e.target.value};
+            }
+            return todo;
+        });
+
+        this.setState({ todoData: newTodo });
+    }
+    handleOnSubmit = (e, id) => {
+        if (e.key === "Enter"){
+            if (this.inputRefs[id]) {
+                this.inputRefs[id].blur();
+            }
+        }
+
     }
     render() {
         return (
@@ -38,18 +65,30 @@ export default class App extends React.Component {
                     <img src="/assets/modify.svg" alt="modify"/>
                     <img src="/assets/delete.svg" alt="delete"/>
                 </div>
-                {this.todoData.map((data)=> (
+                {this.state.todoData.map((data)=> (
                     <div className="add-todos" key={data.id}>
-                        <input type="checkbox" checked={data.completed} onChange={(e) => this.handleOnChangeChecked(e)}/>
-                        <input className="todo" type="text" defaultChecked={false} value={data.title}/>
-                        <img src="/assets/modify.svg" alt="modify" onClick={() => this.handleModify(data.id)}/>
-                        <img src="/assets/delete.svg" alt="delete" onClick={() => this.handleDelete(data.id)}/>
+                        <input type="checkbox"
+                               checked={data.completed}
+                               onChange={(e) => this.handleOnChangeChecked(e, data.id)}/>
+                        <input
+                            className="todo"
+                            type="text"
+                            name="value"
+                            value={data.title}
+                            placeholder="할 일 입력하기"
+                            ref={(ref) => this.inputRefs[data.id] = ref}
+                            onChange={(e) => this.handleOnChangeInput(e, data.id)}
+                            onKeyUp={(e) => this.handleOnSubmit(e, data.id)}
+                        />
+                        <img src="/assets/modify.svg" alt="modify"
+                             onClick={() => this.handleModify(data.id)}
+                        />
+                        <img src="/assets/delete.svg" alt="delete"
+                             onClick={() => this.handleDelete(data.id)}
+                        />
                     </div>
                 ))}
-
             </>
         )
     }
-
-
 }
