a4c1005a2e434719604acaa934155a5ee140fd02
Jae-Hong-Kim <newfly101@naver.com>
2024-12-04 10:55:26 +0900

    [1] state : useState() 사용하게 변경

--- web/src/App.js
+++ web/src/App.js
@@ -1,20 +1,31 @@
 import './css/general.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="todo-header">
-        <h1>Todos 앱</h1>
-        <span className="new-todo">새로운 TODO 추가하기</span>
-      </div>
-      <div className="add-todos">
-        <input type="checkbox"/>
-        <input className="todo" type="text"/>
-        <img src="/assets/modify.svg" alt="modify"/>
-        <img src="/assets/delete.svg" alt="delete"/>
-      </div>
-    </div>
-  );
+    const [todoData, setTodoData] = useState([
+        {
+            count: 2,
+            todoData: [
+                {id: 1, title: 'Todo 1', completed: true},
+                {id: 2, title: 'Todo 2', completed: false}
+            ],
+        }
+    ]);
+    console.log(todoData);
+    return (
+        <div className="App">
+            <div className="todo-header">
+                <h1>Todos 앱</h1>
+                <span className="new-todo">새로운 TODO 추가하기</span>
+            </div>
+            <div className="add-todos">
+                <input type="checkbox"/>
+                <input className="todo" type="text"/>
+                <img src="/assets/modify.svg" alt="modify"/>
+                <img src="/assets/delete.svg" alt="delete"/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
