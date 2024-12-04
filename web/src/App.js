555c068621773e0a0a0b60c165a362eac94e0c4d
Jae-Hong-Kim <newfly101@naver.com>
2024-12-04 11:18:56 +0900

    [1] state 값 onChange()하는 경우에 setState()하게 Spread 연산자 사용해서 값 유지하고 변경하기

--- web/src/App.js
+++ web/src/App.js
@@ -12,6 +12,22 @@ export default function App() {
         }
     );
     console.log(data);
+
+    function handleOnChangeTitle(e, id) {
+        setData((prevState) => ({
+            ...prevState,
+            // count : 이전 state값 그대로 유지
+            todoData: prevState.todoData.map((todo) =>
+                todo.id === id
+                    ? {...todo, title: e.target.value}
+                    : todo
+                // map으로 쪼가르면 {id:1} , {id:2}
+                // id가 변경하는 값과 일치하면 나머지 data는 prevState로 두고, title만 값 변경
+                // id가 변경하는 값과 불일치하면 이전 값 그대로 유지
+            )
+        }));
+    }
+
     return (
         <div className="App">
             <div className="todo-header">
@@ -19,15 +35,21 @@ export default function App() {
                 <span className="new-todo">새로운 TODO 추가하기</span>
             </div>
             {data.todoData.map((item) => {
-                console.log(item);
+                // console.log(item);
                 return (
-                <div className="add-todos" key={item.id}>
-                    <input type="checkbox"/>
-                    <input className="todo" type="text"/>
-                    <img src="/assets/modify.svg" alt="modify"/>
-                    <img src="/assets/delete.svg" alt="delete"/>
-                </div>
-            )})}
+                    <div className="add-todos" key={item.id}>
+                        <input type="checkbox"/>
+                        <input
+                            className="todo"
+                            type="text"
+                            value={item.title}
+                            onChange={(e) => handleOnChangeTitle(e, item.id)}
+                        />
+                        <img src="/assets/modify.svg" alt="modify"/>
+                        <img src="/assets/delete.svg" alt="delete"/>
+                    </div>
+                )
+            })}
         </div>
     );
 }
