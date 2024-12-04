af5b359478073a7911ddec7b8779848d986d029a
Jae-Hong-Kim <newfly101@naver.com>
2024-12-04 11:00:43 +0900

    [1] state 값을 map()으로 출력하는 것을 확인

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,8 @@
 import './css/general.css';
 import {useState} from "react";
 
-function App() {
-    const [todoData, setTodoData] = useState([
+export default function App() {
+    const [data, setData] = useState(
         {
             count: 2,
             todoData: [
@@ -10,22 +10,24 @@ function App() {
                 {id: 2, title: 'Todo 2', completed: false}
             ],
         }
-    ]);
-    console.log(todoData);
+    );
+    console.log(data);
     return (
         <div className="App">
             <div className="todo-header">
                 <h1>Todos 앱</h1>
                 <span className="new-todo">새로운 TODO 추가하기</span>
             </div>
-            <div className="add-todos">
-                <input type="checkbox"/>
-                <input className="todo" type="text"/>
-                <img src="/assets/modify.svg" alt="modify"/>
-                <img src="/assets/delete.svg" alt="delete"/>
-            </div>
+            {data.todoData.map((item) => {
+                console.log(item);
+                return (
+                <div className="add-todos" key={item.id}>
+                    <input type="checkbox"/>
+                    <input className="todo" type="text"/>
+                    <img src="/assets/modify.svg" alt="modify"/>
+                    <img src="/assets/delete.svg" alt="delete"/>
+                </div>
+            )})}
         </div>
     );
 }
-
-export default App;
